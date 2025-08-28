c183c71404682e5846e7b63427b7bd488aa74bc9
Vadim Shakula <vadim.shakula@internet.ru>
2025-08-28 17:23:40 +0000

    20250828v1

--- run_mlflow_server.sh
+++ run_mlflow_server.sh
@@ -1,7 +1,17 @@
+set -Eeuo pipefail
+
+if [[ -f ./.env ]]; then
+  set -a        # всё, что source-им — экспортируется в окружение
+  source ./.env
+  set +a
+else
+  echo "Файл .env не найден рядом со скриптом"; exit 1
+fi
+
 export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_BUCKET_NAME=$S3_BUCKET_NAME
+export AWS_BUCKET_NAME=$AWS_BUCKET_NAME
 
 mlflow server \
   --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
